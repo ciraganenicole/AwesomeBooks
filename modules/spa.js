a0ed178947993370254a664734a85dac0762cf00
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-21 11:01:21 +0100

    SPA feature

---
+++ modules/spa.js
@@ -0,0 +1,29 @@
+document.getElementById('firstBlock').style.display = 'block';
+document.getElementById('secondBlock').style.display = 'none';
+document.getElementById('contact').style.display = 'none';
+
+document.getElementById('first').addEventListener('click', () => {
+    document.getElementById('firstBlock').style.display = 'block';
+    document.getElementById('secondBlock').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('first').style.color = 'blue';
+    document.getElementById('third').style.color = 'black';
+    document.getElementById('second').style.color = 'black';
+}, false);
+
+document.getElementById('second').addEventListener('click', () => {
+    document.getElementById('firstBlock').style.display = 'none';
+    document.getElementById('secondBlock').style.display = 'block';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('second').style.color = 'blue';
+    document.getElementById('third').style.color = 'black';
+    document.getElementById('first').style.color = 'black';
+}, false);
+document.getElementById('third').addEventListener('click', () => {
+    document.getElementById('firstBlock').style.display = 'none';
+    document.getElementById('secondBlock').style.display = 'none';
+    document.getElementById('contact').style.display = 'block';
+    document.getElementById('third').style.color = 'blue';
+    document.getElementById('second').style.color = 'black';
+    document.getElementById('first').style.color = 'black';
+}, false);
