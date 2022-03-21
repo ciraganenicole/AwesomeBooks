b2f5256bd9b9f2bc83316b820cd7d8b6cf012bb3
Ciragane Nicole <ciraganenicole@gmail.com>
2022-03-21 11:01:37 +0100

    Add books to local storage

---
+++ modules/storeBook.js
@@ -0,0 +1,14 @@
+export default class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    getHtml() {
+        return `<ul class="book">
+     <li class="list">"${this.title}" by 
+     ${this.author}</li>
+     <button class="remove">Remove</button>
+     </ul>`;
+    }
+}
